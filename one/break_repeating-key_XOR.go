cc6b10ef328856e5383cdbcf756321c1ba48614f
Blake Griffith <blake.a.griffith@gmail.com>
2016-05-16 14:38:35 -0500

    Finish solution to challenge 6.

--- one/break_repeating-key_XOR.go
+++ one/break_repeating-key_XOR.go
@@ -1,58 +1,246 @@
 package one
 
 import (
-    // keep hex out of here and in tests
-    "fmt"
-    "math"
-    "errors"
+	// keep hex out of here and in tests
+	"errors"
+	"fmt"
+	"sort"
 )
 
-func Xor(a, b []byte) ([]byte, error) {
-    out := make([]byte, len(a))
-    if len(a) != len(b) {
-        return out, errors.New("inputs different length")
-    }
+func P(b interface{}) {
+	fmt.Println(b)
+}
 
-    for i, v := range a {
-        out[i] = v ^ b[i]
-    }
-    return out, nil
+func Xor(a, b []byte) ([]byte, error) {
+	out := make([]byte, len(a))
+	if len(a) != len(b) {
+		return out, errors.New("inputs different length")
+	}
+
+	for i, v := range a {
+		out[i] = v ^ b[i]
+	}
+	return out, nil
 }
 
 func countOnesInByte(a byte) int {
-    pows := []byte{1, 2, 4, 8, 16, 32, 64, 128}
-    out := 0
-    for _, p := range pows {
-        if b := p & a; b == p {
-            out += 1
-        }
-    }
-    return out
+	pows := []byte{1, 2, 4, 8, 16, 32, 64, 128}
+	out := 0
+	for _, p := range pows {
+		if b := p & a; b == p {
+			out += 1
+		}
+	}
+	return out
 }
 
 func HammingDistance(a, b []byte) (int, error) {
-    out := 0
-    if len(a) != len(b) {
-        return 0, errors.New("inputs different length")
-    }
+	out := 0
+	if len(a) != len(b) {
+		return 0, errors.New("inputs different length")
+	}
 
-    difs, _ := Xor(a, b)
-    for _, v := range difs {
-        out += countOnesInByte(v)
-    }
-    return out, nil
+	difs, _ := Xor(a, b)
+	for _, v := range difs {
+		out += countOnesInByte(v)
+	}
+	return out, nil
 }
 
-func ShannonEntropy(bytes []byte) float64 {
-    l := float64(len(bytes))
-    count := make(map[int]int)
-    for b := range bytes {
-        count[b] += 1
-    }
-    out := 0.0
-    for _, v := range count {
-        freq := float64(v) / l
-        out -= freq * math.Log2(freq)
-    }
-    return out
+func HammingsOfData(b []byte) (hammings []float64, keysizes []int) {
+	keysizes = make([]int, 38)
+	// possible keysizes
+	for i := range keysizes {
+		keysizes[i] = i + 2
+	}
+
+	hammings = make([]float64, 38)
+
+	fs := len(b) // filesize
+	for ki, ks := range keysizes {
+		nkb := (fs / ks) - 1 // # of ks blocks
+		hamavg := 0.0
+		hamsum := 0.0
+
+		for i := 0; i < nkb; i++ {
+			s1 := b[ks*i : ks*(i+1)]
+			s2 := b[ks*(i+1) : ks*(i+2)]
+			tmpham, _ := HammingDistance(s1, s2)
+			hamsum += float64(tmpham)
+		}
+		hamavg = hamsum / float64(ks)
+		hamavg = hamavg / float64(nkb-1)
+		hammings[ki] = hamavg
+	}
+
+	sort.Sort(&IndexSort{hammings, keysizes})
+	return hammings, keysizes
+}
+
+type IndexSort struct {
+	sort.Float64Slice
+	idxs []int
+}
+
+func (is *IndexSort) Swap(i, j int) {
+	is.Float64Slice[i], is.Float64Slice[j] = is.Float64Slice[j], is.Float64Slice[i]
+	is.idxs[i], is.idxs[j] = is.idxs[j], is.idxs[i]
+}
+
+func GetKeySizeBlocks(b []byte, ks int) [][]byte {
+	nblocks := len(b) / ks
+	if (len(b) % ks) != 0 {
+		nblocks += 1
+	}
+	blocks := make([][]byte, nblocks)
+	for i := 0; i < nblocks; i++ {
+		if ks*(i+1) < len(b) {
+			blocks[i] = make([]byte, ks)
+			copy(blocks[i], b[i*ks:ks*(i+1)])
+		} else {
+			blocks[i] = make([]byte, len(b)%ks)
+			copy(blocks[i], b[i*ks:len(b)])
+		}
+	}
+	return blocks
+}
+
+func TransposeBlocks(blocks [][]byte) [][]byte {
+	nblocks := len(blocks)
+	ks := len(blocks[0])
+	tblocks := make([][]byte, ks)
+	for i := range tblocks {
+		if i < len(blocks[nblocks-1]) {
+			tblocks[i] = make([]byte, nblocks)
+		} else {
+			tblocks[i] = make([]byte, nblocks-1)
+		}
+		for j := range blocks {
+			if j < len(tblocks[i]) {
+				tblocks[i][j] = blocks[j][i]
+			}
+		}
+	}
+	return tblocks
+}
+
+type ByteCounter struct {
+	Mapping map[byte]int
+	Order   []byte
+}
+
+func (c *ByteCounter) Len() int {
+	return len(c.Mapping)
+}
+
+func (c *ByteCounter) Less(i, j int) bool {
+	return c.Mapping[c.Order[i]] > c.Mapping[c.Order[j]]
+}
+
+func (c *ByteCounter) Swap(i, j int) {
+	c.Order[j], c.Order[i] = c.Order[i], c.Order[j]
+}
+
+func CountBytes(b []byte) *ByteCounter {
+	out := new(ByteCounter)
+	out.Mapping = make(map[byte]int)
+	for _, v := range b {
+		out.Mapping[v] += 1
+	}
+	out.Order = make([]byte, len(out.Mapping))
+	i := 0
+	for k := range out.Mapping {
+		out.Order[i] = k
+		i++
+	}
+	sort.Sort(out)
+	return out
+}
+
+func isPrintable(b byte) bool {
+	if (127 >= b) && (b >= 32) {
+		return true
+	}
+	if b == 10 { // newline
+		return true
+	} else {
+		return false
+	}
+}
+
+func AllPrintable(arr []byte) bool {
+	for _, v := range arr {
+		if !isPrintable(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func FindXorChar(b []byte) (byte, error) {
+	common_chars := []byte(" aoeuidhtns")
+	count := CountBytes(b)
+
+	for _, u := range count.Order {
+		for _, v := range common_chars {
+			x := u ^ v
+			xord := VectorXor([]byte{x}, b)
+			if AllPrintable(xord) && isPrintable(x) {
+				return x, nil
+			}
+		}
+	}
+	return uint8(0), errors.New("xor char not found")
+}
+
+func BuildKey(ks int, tblocks [][]byte) ([]byte, error) {
+	key := make([]byte, ks)
+	for i := range tblocks {
+		x, err := FindXorChar(tblocks[i])
+		key[i] = x
+		if err != nil {
+			return key, err
+		}
+	}
+	return key, nil
+}
+
+func VectorXor(short []byte, long []byte) []byte {
+	ls := len(short)
+	ll := len(long)
+	nshorts := ll / ls
+	if ll%ls != 0 {
+		nshorts += 1
+	}
+	out := make([]byte, ll)
+	for i := 0; i < nshorts; i++ {
+		if ls*(i+1) <= ll {
+			copy(out[ls*i:ls*(i+1)], short)
+		} else {
+			copy(out[ls*i:ll], short[:ll%ls])
+		}
+	}
+	out, _ = Xor(out, long)
+	return out
+}
+
+func makeBlocks(ks int, data []byte) [][]byte {
+	blocks := GetKeySizeBlocks(data, ks)
+	tblocks := TransposeBlocks(blocks)
+	return tblocks
+}
+
+func CrackVigenere(data []byte) ([]byte, []byte, error) {
+	var output []byte
+	var k []byte
+	_, keysizes := HammingsOfData(data)
+
+	for _, ks := range keysizes {
+		blocks := makeBlocks(ks, data)
+		k, err := BuildKey(ks, blocks)
+		if err == nil {
+			return VectorXor(k, data), k, nil
+		}
+	}
+	return output, k, errors.New("couldn't crack it")
 }
--- one/break_repeating-key_XOR_test.go
+++ one/break_repeating-key_XOR_test.go
@@ -1,58 +1,258 @@
 package one
+
 import (
-    "testing"
-    "encoding/hex"
+	"bytes"
+	"encoding/base64"
+	"encoding/hex"
+	"io/ioutil"
+	"testing"
 )
+
 func testEq(a, b []byte) bool {
 
-    if a == nil && b == nil {
-        return true;
-    }
+	if a == nil && b == nil {
+		return true
+	}
 
-    if a == nil || b == nil {
-        return false;
-    }
+	if a == nil || b == nil {
+		return false
+	}
 
-    if len(a) != len(b) {
-        return false
-    }
+	if len(a) != len(b) {
+		return false
+	}
 
-    for i := range a {
-        if a[i] != b[i] {
-            return false
-        }
-    }
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
 
-    return true
+	return true
 }
 
-func TestXor(t * testing.T) {
-    in1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
-    in2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
-    out, _ := hex.DecodeString("746865206b696420646f6e277420706c6179")
-
-    res, err := Xor(in1, in2)
-
-    if err != nil {
-        t.Error(err)
-    }
-
-    if !testEq(res, out) {
-        t.Fatal("fail")
-    }
+func assert(a, b []byte) {
+	x := testEq(a, b)
+	if !x {
+		panic("assert fail")
+	}
 }
 
-func TestHamming(t * testing.T) {
-    in1 := []byte("this is a test")
-    in2 := []byte("wokka wokka!!!")
-    out := 37
+func TestXor(t *testing.T) {
+	in1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	in2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+	out, _ := hex.DecodeString("746865206b696420646f6e277420706c6179")
 
-    res, err := HammingDistance(in1, in2)
-    if err != nil {
-        t.Error(err)
-    }
+	res, err := Xor(in1, in2)
 
-    if res != out {
-        t.Fatal(res, out)
-    }
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !testEq(res, out) {
+		t.Fatal("fail")
+	}
+}
+
+func TestHamming(t *testing.T) {
+	in1 := []byte("this is a test")
+	in2 := []byte("wokka wokka!!!")
+	out := 37
+
+	res, err := HammingDistance(in1, in2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res != out {
+		t.Fatal(res, out)
+	}
+}
+
+/*
+func TestKeySizes(t * testing.T) {
+    file_bytes, _ := ioutil.ReadFile("data6.txt")
+    // strip \n
+    file_bytes = bytes.Replace(file_bytes, []byte("\n"), []byte(""), -1)
+    FindKeySize(file_bytes)
+}
+*/
+
+func TestAllPrintable(t *testing.T) {
+	in := []byte("All thesee chars are printabel")
+	if !AllPrintable(in) {
+		t.Fatal()
+	}
+}
+
+func TestMyVig(t *testing.T) {
+    var plaintext_n []byte = []byte(`
+    Lol this songs is funny
+    As he came into the window
+    Was a sound of a crescendo
+    He came into her apartment
+    He left the bloodstains on the carpet
+    She was sitting at the table
+    He could see she was unable
+    So she ran into the bedroom
+    She was struck down
+    It was her doom
+    Annie, are you OK
+    Are you OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    You OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    Will you tell us that you're OK
+    There's a sign at the window
+    That he struck you
+    A crescendo, Annie
+    He came into your apartment
+    He left the bloodstains on the carpet
+    Then you ran into the bedroom
+    You were struck down
+    It was your doom
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    You've been hit by
+    You've been struck by
+    A smooth criminal
+    So they came into the outway
+    It was Sunday
+    What a black day
+    I could feel your salutation
+    Sounding heartbeats
+    Intimidations
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    Will you tell us that you're OK
+    There's a sign at the window
+    That he struck you
+    A crescendo, Annie
+    He came into your apartment
+    He left the bloodstains on the carpet
+    Then you ran into the bedroom
+    You were struck down
+    It was your doom
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    You've been hit by
+    You've been struck by
+    A smooth criminal
+    Annie, are you OK
+    Will you tell us that you're OK
+    There's a sign at the window
+    That he struck you
+    A crescendo, Annie
+    He came into your apartment
+    He left the bloodstains on the carpet
+    Then you ran into the bedroom
+    You were struck down
+    It was your doom
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    Annie, are you OK
+    You OK
+    Are you OK, Annie
+    As he came into the window
+    Was a sound of a crescendo
+    He came into her apartment
+    He left the bloodstains on the carpet
+    She was sitting at the table
+    He could see she was unable
+    So she ran into the bedroom
+    She was struck down
+    It was her doom
+    Annie, are you OK
+    Are you OK
+    Are you OK, Annie
+    Annie, are you OK
+    lol`)
+    var plaintext []byte = bytes.Replace(plaintext_n, []byte("\n"), []byte(""), -1)
+
+    var key []byte = []byte("Alien ant farm")
+    var ciphertext []byte = VectorXor(key, plaintext)
+
+	pt1 := []byte("foobarqux")
+	key1 := []byte("xofoo")
+	ct1 := VectorXor(key1, pt1)
+	res1 := VectorXor(key1, ct1)
+	if string(res1) != string(pt1) {
+		t.Fatal()
+	}
+
+	res2 := VectorXor(key, ciphertext)
+	if string(res2) != string(plaintext) {
+		t.Fatal()
+	}
+
+	res3 := VectorXor(key, ciphertext[:len(key)])
+	if !testEq(res3, plaintext[:len(key)]) {
+		t.Fatal()
+	}
+
+	ct2 := make([]byte, 20)
+	for i := range ct2 {
+		ct2[i] = plaintext[i]
+		ct2[i] ^= uint8('a')
+	}
+	res4 := VectorXor([]byte{'a'}, ct2)
+	if !testEq(res4, plaintext[:20]) {
+		t.Fatal()
+	}
+
+	r, _, _ := CrackVigenere(ciphertext)
+	if !testEq(r, plaintext) {
+		t.Fatal()
+	}
+}
+
+func TestCryptopalskVigenere(t *testing.T) {
+	file_bytes, _ := ioutil.ReadFile("data6.txt")
+	file_bytes, err := base64.StdEncoding.DecodeString(string(file_bytes))
+	if err != nil {
+		t.Fatal()
+	}
+
+	_, k, err := CrackVigenere(file_bytes)
+	if err != nil {
+		t.Fatal()
+	}
+
+	if string(k) != "Terminator X: Bring the noise" {
+		t.Fatal()
+	}
 }
