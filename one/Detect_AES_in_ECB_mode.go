817348c2976774fae6b148eb4e33f2df43871cb4
Blake Griffith <blake.a.griffith@gmail.com>
2016-05-16 16:19:11 -0500

    Add solution to challenge 8 in go.

---
+++ one/Detect_AES_in_ECB_mode.go
@@ -0,0 +1,26 @@
+package one
+
+func DetectECB(blocksize int, ciphertext []byte) bool {
+    counter := make(map[string]int)
+
+    ctlen := len(ciphertext)
+    nblocks := ctlen/blocksize
+    if ctlen % blocksize != 0 {
+        nblocks += 1
+    }
+
+    for i := 0; i < nblocks; i++ {
+        ll := blocksize * i
+        ul := blocksize * (i + 1)
+        if ul > ctlen {
+            ul = ctlen
+        }
+
+        k := string(ciphertext[ll:ul])
+        counter[k] += 1
+        if counter[k] > 1 {
+            return true
+        }
+    }
+    return false
+}
---
+++ one/Detect_AES_in_ECB_mode_test.go
@@ -0,0 +1,28 @@
+package one
+
+import (
+    "testing"
+    "io/ioutil"
+    "encoding/base64"
+    "bytes"
+)
+func TestDetectECB(t * testing.T) {
+    file_bytes, _ := ioutil.ReadFile("data8.txt")
+    encoded := bytes.Split(file_bytes, []byte("\n"))
+    ciphertexts := make([][]byte, len(encoded))
+    for i, e := range encoded {
+        d, _ := base64.StdEncoding.DecodeString(string(e))
+        ciphertexts[i] = d
+    }
+
+    blocksize := 16
+    ecb_ct := 0
+    for i, ct := range ciphertexts {
+        if DetectECB(blocksize, ct) {
+            ecb_ct = i
+        }
+    }
+    if ecb_ct != 132 {
+        t.Fatal()
+    }
+}
