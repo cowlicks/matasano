a6dab164aa07da900424d6758f87bd24b007908b
Blake Griffith <blake.a.griffith@gmail.com>
2016-05-18 14:01:12 -0500

    Add CBC en/decrypt

---
+++ aesmodes/CBC.go
@@ -0,0 +1,86 @@
+package aesmodes
+
+import (
+    "../xor"
+    "../padding"
+    "errors"
+    "crypto/aes"
+)
+
+func EncryptCBC(key, plaintext []byte) ([]byte, error) {
+    errout := []byte("")
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return errout, err
+    }
+    bs := block.BlockSize()
+    plaintext, err = padding.Pad(bs, plaintext)
+    if err != nil {
+        return errout, err
+    }
+
+    iv := make([]byte, bs)
+    ciphertext := make([]byte, bs + len(plaintext))
+    copy(ciphertext[:bs], iv)
+
+    nblocks := 1 + (len(plaintext)/bs)
+
+    xorblock, err := xor.Xor(iv, plaintext[:bs])
+    if err != nil {
+        return errout, err
+    }
+    for i := 1; i < nblocks; i++ {
+        ciphertextblock := ciphertext[bs*i:bs*(i + 1)]
+        block.Encrypt(ciphertextblock, xorblock)
+        if (i != nblocks - 1) {
+            plaintextblock := plaintext[bs*i: bs*(i + 1)]
+            xorblock, err = xor.Xor(ciphertextblock, plaintextblock)
+            if err != nil {
+                return errout, err
+            }
+        }
+    }
+    return ciphertext, nil
+}
+
+func DecryptCBC(key, ciphertext []byte) ([]byte, error) {
+    errout := []byte("")
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return errout, err
+    }
+    bs := block.BlockSize()
+
+    iv := make([]byte, bs)
+    copy(iv, ciphertext[:bs])
+    ciphertext = ciphertext[bs:] // remove iv from ciphertext
+
+    ctsize := len(ciphertext)
+    nblocks := ctsize / bs
+
+    if ctsize % bs != 0 {
+        return errout, errors.New("Ciphertext not an integer multiple of blocksize")
+    }
+
+    xorblock := make([]byte, bs)
+    decblock := make([]byte, bs)
+    plaintext := make([]byte, ctsize)
+
+    for i := 0; i < nblocks ; i++ {
+        block.Decrypt(decblock, ciphertext[bs*i:bs*(i + 1)])
+        if i == 0 {
+            xorblock, err = xor.Xor(iv, decblock)
+        } else {
+            xorblock, err = xor.Xor(ciphertext[bs*(i - 1):bs*i], decblock)
+        }
+        if err != nil {
+            return errout, err
+        }
+        copy(plaintext[bs*i:bs*(i + 1)], xorblock)
+    }
+    plaintext, err = padding.UnPad(bs, plaintext)
+    if err != nil {
+        return errout, err
+    }
+    return plaintext, nil
+}
---
+++ aesmodes/CBC_test.go
@@ -0,0 +1,40 @@
+package aesmodes
+
+import (
+    "../util"
+    "testing"
+    "io/ioutil"
+    "encoding/base64"
+)
+
+func TestCBC(t * testing.T) {
+    key := []byte("YELLOW SUBMARINE")
+
+    file_bytes, _ := ioutil.ReadFile("../data/data7.txt")
+
+    decodelen := base64.StdEncoding.DecodedLen(len(file_bytes))
+    ciphertext := make([]byte, decodelen)
+    ctlen, _ := base64.StdEncoding.Decode(ciphertext, file_bytes)
+    ciphertext = ciphertext[:ctlen]
+
+    plaintext, err := DecryptECB(key, ciphertext)
+    if err != nil {
+        t.Fatal()
+    }
+    exp := []byte("I'm back and I'm ringin' the bell")
+    if !util.ByteEq(plaintext[:len(exp)], exp) {
+        t.Fatal()
+    }
+
+    new_ciphertext, err := EncryptCBC(key, plaintext)
+    if err != nil {
+        t.Fatal()
+    }
+    new_plaintext, err := DecryptCBC(key, new_ciphertext)
+    if err != nil {
+        t.Fatal()
+    }
+    if !util.ByteEq(new_plaintext, plaintext) {
+        t.Fatal()
+    }
+}
