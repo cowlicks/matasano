a8fb70e79d79333260daf06d198f6da8422da372
Blake Griffith <blake.a.griffith@gmail.com>
2016-08-15 18:58:26 -0700

    More WIP challenge 12.

--- twelve/twelve.go
+++ twelve/twelve.go
@@ -1,20 +1,12 @@
-package main
+package twelve
 
 import (
-    "fmt"
     "encoding/base64"
-    "crypto/aes"
     "../aesmodes"
 )
 
 var key, _ = aesmodes.MakeKey()
 var appendme, decodeerr  = base64.StdEncoding.DecodeString("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
-
-var aesblock, err = aes.NewCipher(key)
-
-func P(b interface{}) {
-	fmt.Println(b)
-}
 
 func Encryptor(plaintext []byte, key []byte) []byte {
     input := append(plaintext, appendme...)
@@ -22,50 +14,40 @@ func Encryptor(plaintext []byte, key []byte) []byte {
     return out
 }
 
-func EncryptorInspector(encryptor func([]byte, []byte) []byte, key []byte) (int, int) {
-    /*
-    This gets two useful pieces of info about the encrypto, the blocksize.
-    And the smallest size of input to make the output get an extra block.
-    */
-    inputsize := 0
-    init_output_size := len(encryptor(make([]byte, inputsize), key))
-    new_output_size := init_output_size
-    for ;init_output_size == new_output_size;{
-        inputsize++
-        new_output_size = len(encryptor(make([]byte, inputsize), key))
-    }
-    blocksize := new_output_size - init_output_size
-    appendsize := init_output_size - inputsize
-    return blocksize, inputsize
+func KeyedEncryptor(plaintext []byte) []byte {
+    return Encryptor(plaintext, key)
 }
 
-func makeByteDictionary(prefix []byte) map[[16]byte][]byte {
-    if len(prefix) < blocksize - 1 {
-        padsize = blocksize - 1 - len(prefix)
-        tmp = make([]byte, blocksize)
-        prefix = append(make([]byte, padsize), prefix...)
+func FindBlockSize(kencryptor func([]byte) []byte) int {
+    bs := 0
+    for padlen := 0; bs == 0; padlen++ {
+        init := len(kencryptor(make([]byte, 0)))
+        longpad := make([]byte, padlen)
+        final := len(kencryptor(longpad))
+        bs = final - init
     }
+    return bs
+}
 
-    out := make(map[[16]byte][]byte)
+func CheckEncryptorECB(kencryptor func([]byte) []byte) bool {
+    bs := FindBlockSize(kencryptor) // maybe make this an arg
+    block := make([]byte, bs)
+    ct := kencryptor(append(block, block...))
+    for i := 0; i < bs; i++ {
+        if ct[i] != ct[i + bs] {
+            return false
+        }
+    }
+    return true
+}
+
+func MakeDict(bs int) map[string][]byte {
+    out := make(map[string][]byte)
     for i := 0; i < 256; i++ {
-        justshort := make([]byte, blocksize)
-        ctblock := make([]byte, blocksize)
-        justshort[blocksize - 1] = i
-        aesblock.Encrypt(ctblock, justshort)
-        out[ctblock] = justshort
+        block := make([]byte, bs)
+        block[bs - 1] = byte(i)
+        ct := KeyedEncryptor(block)
+        out[string(block[:bs])] = ct[:bs]
     }
     return out
-}
-
-/*
-func getFirstByte(inputsize) byte {
-    justshort = make([]byte, blocksize - 1)
-    ct = Encryptor(justshort, key)
-    ct[0] = 
-*/
-
-func main() {
-    blocksize, inputsize := EncryptorInspector(Encryptor, key)
-    /* get first byte */
-
 }
--- twelve/twelve_test.go
+++ twelve/twelve_test.go
@@ -1,11 +1,34 @@
-package main
+package twelve
 
 import (
+    "fmt"
     "testing"
+    "../aesmodes"
 )
 
 func TestOracle(t * testing.T) {
-    MakeKey()
-    c, _ := OracleEncryptor(make([]byte, 300))
-    OracleDetector(c)
+    // test encryptor
+    key, _ := aesmodes.MakeKey()
+    Encryptor([]byte("manifolds"), key)
+
+    // test keyedencryptor
+    KeyedEncryptor([]byte("socks"))
+
+    // test get blocksize
+    bs := FindBlockSize(KeyedEncryptor)
+    if bs != 16 {
+        t.Fatal()
+    }
+
+    // test check ecb
+    if !CheckEncryptorECB(KeyedEncryptor) {
+        t.Fatal()
+    }
+
+    // test make dict
+    yo := MakeDict(16)
+    for k, v := range yo {
+        fmt.Println(k)
+        fmt.Println(v)
+    }
 }
