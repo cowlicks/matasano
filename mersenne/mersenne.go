fff3366e14dab4187e2eb9c8efbde6270cf025b2
Blake Griffith <blake.a.griffith@gmail.com>
2016-09-09 22:21:23 -0700

    WIP at cloning a MT19937

--- mersenne/mersenne.go
+++ mersenne/mersenne.go
@@ -1,5 +1,15 @@
 package mersenne
 
+import (
+    "strconv"
+    "fmt"
+    "../util"
+)
+
+func PrintBin(a uint) {
+    fmt.Println(strconv.FormatUint(uint64(a), 2))
+}
+
 type Mersenne struct {
 	w, n, m, r, a, u, d, s, b, t, c, l, f uint
 	index                                 uint
@@ -15,12 +25,15 @@ func NewMersenne19937(seed uint) *Mersenne {
 		n:     n,               // degree of recurrence
 		m:     397,             // middle word
 		r:     31,              // seperation point of one word
-		a:     2567483615,      // coefficients of twist matrix
-		d:     4294967295,      // --
-		b:     2636928640,      //   \
-		c:     4022730752,      //    -- bit shifting masks
-		l:     18,              //   /
-		f:     1812433253,      // --
+		a:     0x9908B0DF,      // coefficients of twist matrix (wtf wikipedia)
+        u:     11,              // -- 
+        s:     7,               //   \__ bit shifts
+        t:     15,              //   /
+		l:     18,              // --
+		d:     0xFFFFFFFF,      // --
+		b:     0x9D2C5680,      //   \__ bit masks
+		c:     0xEFC60000,      // __/
+		f:     1812433253,      // initialization constant
 		index: n,               // state holders
 		state: make([]uint, n), // ''
 	}
@@ -38,12 +51,17 @@ func (mt *Mersenne) Next() uint32 {
 	}
 
 	y := mt.state[mt.index]
-	y = y ^ ((y >> mt.u) & mt.d)
-	y = y ^ ((y << mt.s) & mt.b)
-	y = y ^ ((y << mt.t) & mt.c)
-	y = y ^ (y >> mt.l)
+    y = mt.temper(y)
 	mt.index++
 	return uint32(y & (uint(^uint32(0))))
+}
+
+func (mt *Mersenne) temper(y uint) uint {
+    y = y ^ ((y >> mt.u) & mt.d)
+    y = y ^ ((y << mt.s) & mt.b)
+    y = y ^ ((y << mt.t) & mt.c)
+    y = y ^ (y >> mt.l)
+    return y
 }
 
 func (mt *Mersenne) twist() {
@@ -56,13 +74,89 @@ func (mt *Mersenne) twist() {
 		if y%2 != 0 {
 			mt.state[i] = mt.state[i] ^ mt.a
 		}
-		mt.index = 0
 	}
+    util.P(mt.state[:5])
+    mt.index = 0
 }
 
-func (mt *Mersenne) untwist() {
-    for i := uint(mt.n - 1); i <= 0; i-- {
-        x := mt.state[i] ^ mt.a
+/*
+These numbers are a mystery to me.
 
+The operations in the temper function, actually represent a matrix
+multiplication with a vector.  The matrix is a constant tempering matrix, the
+vector is the uint (each bit is an element in the vector.  Somehow this
+operation is reduced to the bitshifting and &'ing in the temper function. Idk
+how this translation happens.  But the tempering matrix is invertible, so the
+result of the temper function can be multiplied by the inverse of the tempering
+matrix to get the original value. This untemper function apparently does this.
+Its values are magic to me and they are based on the constants for MT19937.
+This was taken from:
+
+https://www.randombit.net/bitbashing/2009/07/21/inverting_mt19937_tempering.html# 
+
+The matrices themselves and another untempering formula can be found here:
+
+https://gist.github.com/oupo/ce045423a15395d31d3c 
+*/
+func untemper(y uint) uint {
+    y = y ^ (y >> 18)
+    y = y ^ ((y << 15) & 0xEFC60000)
+    y = y ^ ((y << 7) & 0x1680)
+    y = y ^ ((y << 7) & 0xC4000)
+    y = y ^ ((y << 7) & 0xD200000)
+    y = y ^ ((y << 7) & 0x90000000)
+    y = y ^ ((y >> 11) & 0xFFC00000)
+    y = y ^ ((y >> 11) & 0x3FF800)
+    y = y ^ ((y >> 11) & 0x7FF)
+    return y
+}
+
+func untemper2(y uint) uint {
+    y = y ^ (y >> 18)
+    y = y ^ ((y << 15) & 0xEFC60000)
+    y = y ^ ((y << 7) & 0x1680)
+    y = y ^ ((y << 7) & 0xC4000)
+    y = y ^ ((y << 7) & 0xD200000)
+    y = y ^ ((y << 7) & 0x90000000)
+    y = y ^ ((y >> 11) & 0xFFC00000)
+    y = y ^ ((y >> 11) & 0x3FF800)
+    y = y ^ ((y >> 11) & 0x7FF)
+    return y
+}
+
+func CloneMT19937(mt *Mersenne) *Mersenne {
+    mtout := make([]uint, mt.n)
+    state_copy := make([]uint, mt.n)
+
+    for ;mt.index != 624; {
+        mt.Next()
     }
+    for i := uint(0); i < mt.n; i++ {
+        mtout[i] = uint(mt.Next())
+    }
+    util.P(mt.index)
+    for i := uint(0); i < mt.n; i++ {
+        state_copy[i] = untemper(mtout[i])
+    }
+	copied_mt := Mersenne{
+		w:     32,              // word size
+		n:     mt.n,               // degree of recurrence
+		m:     397,             // middle word
+		r:     31,              // seperation point of one word
+		a:     0x9908B0DF,      // coefficients of twist matrix (wtf wikipedia)
+        u:     11,              // -- 
+        s:     7,               //   \__ bit shifts
+        t:     15,              //   /
+		l:     18,              // --
+		d:     0xFFFFFFFF,      // --
+		b:     0x9D2C5680,      //   \__ bit masks
+		c:     0xEFC60000,      // __/
+		f:     1812433253,      // initialization constant
+		index: 0,               // state holders
+		state: make([]uint, mt.n), // ''
+	}
+    copy(copied_mt.state, state_copy)
+    util.P(copied_mt.state[:5])
+    copied_mt.index = 0
+    return &copied_mt
 }
--- mersenne/mersenne_test.go
+++ mersenne/mersenne_test.go
@@ -2,16 +2,27 @@ package mersenne
 
 import (
 	"testing"
+    //"../util"
 )
 
+/*
 func TestMersenne(t *testing.T) {
-	mt := NewMersenne19937(666)
-	for i := 0; i < 9999; i++ {
-		mt.Next()
+	mt := NewMersenne19937(5489)
+	for i := 0; i < 3; i++ {
+		util.P(mt.Next())
 	}
 }
 
 func TestTwentyTwo(t *testing.T) {
 	MTFromTime()
-    Crack(MakeMTNumberAtRandomTime())
+//    Crack(MakeMTNumberAtRandomTime())
+}
+*/
+
+func TestCloneMT(t *testing.T) {
+    mt := NewMersenne19937(5489)
+    //mt.Next()
+//    copied_mt := CloneMT19937(mt)
+    CloneMT19937(mt)
+ //   util.P(copied_mt.Next())
 }
