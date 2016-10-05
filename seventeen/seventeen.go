df689564d474e41d46a0cd5efbe15411e26b91c8
Blake Griffith <blake.a.griffith@gmail.com>
2016-10-04 17:33:01 -0700

    gofmt and clean up padding oracle

--- seventeen/seventeen.go
+++ seventeen/seventeen.go
@@ -1,162 +1,156 @@
 package sixteen
 
 import (
-    //"bytes"
-    "fmt"
-    "../padding"
-    "../aesmodes"
+	"../aesmodes"
+	"../padding"
 )
 
 var key, _ = aesmodes.MakeKey()
 
 var bs = 16
 
-func P(in ...interface{}) {
-    fmt.Println(in)
-}
-
 func Encrypt(data []byte) []byte {
-    ct, _ := aesmodes.EncryptCBC(key, data)
-    return ct
+	ct, _ := aesmodes.EncryptCBC(key, data)
+	return ct
 }
 
 func Pb(in [][]byte) {
-    for i := range in {
-        P(in[i])
-    }
+	for i := range in {
+		P(in[i])
+	}
 }
 
 func Oracle(input []byte) bool {
-    _, err := aesmodes.DecryptCBC(key, input)
-    if err == padding.InvalidPad {
-        return false  // bad pad
-    }
-    return true  // good pad
+	_, err := aesmodes.DecryptCBC(key, input)
+	if err == padding.InvalidPad {
+		return false // bad pad
+	}
+	return true // good pad
 }
 
 func GetBlocks(ct []byte) [][]byte {
-    out := make([][]byte, len(ct)/bs)
-    for i := 0; i < len(ct) / bs; i++ {
-        out[i] = ct[bs*i:bs*(i + 1)]
-    }
-    return out
+	out := make([][]byte, len(ct)/bs)
+	for i := 0; i < len(ct)/bs; i++ {
+		out[i] = ct[bs*i : bs*(i+1)]
+	}
+	return out
 }
 
 func CombineBlocks(blocks [][]byte) []byte {
-    ctlen := 0
-    for i := range blocks {
-        ctlen += len(blocks[i])
-    }
-    out := make([]byte, ctlen)
-    for i := range blocks {
-        copy(out[bs*i:bs*(i + 1)], blocks[i])
-    }
-    return out
+	ctlen := 0
+	for i := range blocks {
+		ctlen += len(blocks[i])
+	}
+	out := make([]byte, ctlen)
+	for i := range blocks {
+		copy(out[bs*i:bs*(i+1)], blocks[i])
+	}
+	return out
 }
 
-func OneByte(bite int, blocknum int, solved []byte, blocks[][]byte) int {
-    block := blocks[blocknum]
-    orig_block := make([]byte, bs)
-    copy(orig_block, block)
+func OneByte(bite int, blocknum int, solved []byte, blocks [][]byte) int {
+	block := blocks[blocknum]
+	orig_block := make([]byte, bs)
+	copy(orig_block, block)
 
-    for i := bite + 1; i < bs; i++ {
-        block[i] = block[i] ^ solved[i]
-    }
-    for i := bite; i < bs; i++ {
-        block[i] = block[i] ^ byte(bs - bite)
-    }
+	for i := bite + 1; i < bs; i++ {
+		block[i] = block[i] ^ solved[i]
+	}
+	for i := bite; i < bs; i++ {
+		block[i] = block[i] ^ byte(bs-bite)
+	}
 
-    before_guess := make([]byte, bs)
-    copy(before_guess, block)
-    for guess := 0; guess < 256; guess++ {
-        block[bite] = block[bite] ^ byte(guess)
+	before_guess := make([]byte, bs)
+	copy(before_guess, block)
+	for guess := 0; guess < 256; guess++ {
+		block[bite] = block[bite] ^ byte(guess)
 
-        if Oracle(CombineBlocks(blocks[:blocknum + 2])) {
-            copy(block, orig_block)
-            return guess
-        }
-        copy(block, before_guess)
-    }
-    copy(block, orig_block)
-    panic("missed byte")
-    return -1
+		if Oracle(CombineBlocks(blocks[:blocknum+2])) {
+			copy(block, orig_block)
+			return guess
+		}
+		copy(block, before_guess)
+	}
+	copy(block, orig_block)
+	panic("missed byte")
+	return -1
 }
 
 func OneBlock(blocknum int, blocks [][]byte) []byte {
-    block := blocks[blocknum]
+	block := blocks[blocknum]
 
-    out_block := make([]byte, bs)
+	out_block := make([]byte, bs)
 
-    orig_block := make([]byte, bs)
-    copy(orig_block, block)
+	orig_block := make([]byte, bs)
+	copy(orig_block, block)
 
-    for bite := bs - 1; bite >= 0; bite-- {
-        ptbyte := OneByte(bite, blocknum, out_block, blocks)
-        out_block[bite] = byte(ptbyte)
-    }
-    return out_block
+	for bite := bs - 1; bite >= 0; bite-- {
+		ptbyte := OneByte(bite, blocknum, out_block, blocks)
+		out_block[bite] = byte(ptbyte)
+	}
+	return out_block
 }
 
 func LastBlock(blocks [][]byte) []byte {
-    block := blocks[len(blocks) - 2]
+	block := blocks[len(blocks)-2]
 
-    out := make([]byte, bs)
+	out := make([]byte, bs)
 
-    orig_block := make([]byte, bs)
-    copy(orig_block, block)
+	orig_block := make([]byte, bs)
+	copy(orig_block, block)
 
-    result := 1
+	result := 1
 
-    block[bs - 1] = block[bs - 1] ^ byte(1)
-    pre_guess := make([]byte, bs)
-    copy(pre_guess, block)
-    for guess := 1; guess <= bs; guess++ {
-        copy(block, pre_guess)
-        if guess == 1 {
-            continue
-        }
-        block[bs - 1] = block[bs - 1] ^ byte(guess)
-        if Oracle(CombineBlocks(blocks)) {
-            result = guess
-            break
-        }
-    }
+	block[bs-1] = block[bs-1] ^ byte(1)
+	pre_guess := make([]byte, bs)
+	copy(pre_guess, block)
+	for guess := 1; guess <= bs; guess++ {
+		copy(block, pre_guess)
+		if guess == 1 {
+			continue
+		}
+		block[bs-1] = block[bs-1] ^ byte(guess)
+		if Oracle(CombineBlocks(blocks)) {
+			result = guess
+			break
+		}
+	}
 
-    if result == 0 {
-        return out
-    }
-    for i := 0; i < result; i++ {
-        out[bs - 1 - i] = byte(result)
-    }
+	if result == 0 {
+		return out
+	}
+	for i := 0; i < result; i++ {
+		out[bs-1-i] = byte(result)
+	}
 
-    copy(block, orig_block)
-    for bite := bs - result - 1; bite >= 0; bite-- {
-        ptbyte := OneByte(bite, len(blocks) - 2, out, blocks)
-        out[bite] = byte(ptbyte)
+	copy(block, orig_block)
+	for bite := bs - result - 1; bite >= 0; bite-- {
+		ptbyte := OneByte(bite, len(blocks)-2, out, blocks)
+		out[bite] = byte(ptbyte)
 
-    }
-    return out
+	}
+	return out
 }
 
 func PaddingOracleDecrypt(ct []byte) []byte {
-    blocks := GetBlocks(ct)
-    out := make([]byte, len(ct) - bs)
+	blocks := GetBlocks(ct)
+	out := make([]byte, len(ct)-bs)
 
-    for i := range blocks {
-        var out_block []byte
-        if i == len(blocks) - 2 {
-            out_block = LastBlock(blocks)
-            var err error
-            out_block, err = padding.UnPad(bs, out_block)
-            if err != nil {
-                panic("???")
-            }
-        } else if i == len(blocks) - 1 {
-            continue
-        } else {
-            out_block = OneBlock(i, blocks)
-        }
-        out = append(out, out_block...)
-    }
-    return out
+	for i := range blocks {
+		var out_block []byte
+		if i == len(blocks)-2 {
+			out_block = LastBlock(blocks)
+			var err error
+			out_block, err = padding.UnPad(bs, out_block)
+			if err != nil {
+				panic("???")
+			}
+		} else if i == len(blocks)-1 {
+			continue
+		} else {
+			out_block = OneBlock(i, blocks)
+		}
+		out = append(out, out_block...)
+	}
+	return out
 }
--- seventeen/seventeen_test.go
+++ seventeen/seventeen_test.go
@@ -1,38 +1,35 @@
 package sixteen
 
 import (
-    "encoding/base64"
-    "testing"
+	"encoding/base64"
+	"testing"
 )
 
-
 var encoded_data = []string{
- "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
- "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
- "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
- "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
- "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
- "MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
- "MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
- "MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
- "MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
- "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93"}
-
-
+	"MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
+	"MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
+	"MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
+	"MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
+	"MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
+	"MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
+	"MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
+	"MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
+	"MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
+	"MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93"}
 
 func get_data() [][]byte {
-    data := make([][]byte, len(encoded_data))
-    for i := range encoded_data {
-        data[i], _ = base64.StdEncoding.DecodeString(encoded_data[i])
-    }
-    return data
+	data := make([][]byte, len(encoded_data))
+	for i := range encoded_data {
+		data[i], _ = base64.StdEncoding.DecodeString(encoded_data[i])
+	}
+	return data
 }
 
-func TestSeventeen(t * testing.T) {
-    data := get_data()
-    for i := range data {
-        ct := Encrypt(data[i])
-        out := PaddingOracleDecrypt(ct)
-        P("gotem :",string(out))
-    }
+func TestSeventeen(t *testing.T) {
+	data := get_data()
+	for i := range data {
+		ct := Encrypt(data[i])
+		out := PaddingOracleDecrypt(ct)
+		P("gotem :", string(out))
+	}
 }
