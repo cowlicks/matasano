8229ee4f03e0b9e2b36201a06e9c2c12d4a7371d
Blake Griffith <cowlicks@riseup.net>
2016-12-11 00:22:35 -0800

    Add dh MiTM

--- dh/dh.go
+++ dh/dh.go
@@ -1,27 +1,21 @@
 package dh
 
 import (
+	"../aesmodes"
 	"crypto/rand"
 	"crypto/sha256"
-	"math/big"
 	"encoding/hex"
+	"math/big"
 )
 
 var p *big.Int
 var g = big.NewInt(2)
 
 func init() {
-	phex := `ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
-	e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
-	3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec
-	6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f
-	24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361
-	c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
-	bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff fffffffffffff`
+	phex := `ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff`
 	pbytes, _ := hex.DecodeString(phex)
 	p = new(big.Int).SetBytes(pbytes)
 }
-
 
 func Pow(base, exp, mod *big.Int) *big.Int {
 	return new(big.Int).Exp(base, exp, mod)
@@ -35,4 +29,38 @@ func Rand(mod *big.Int) *big.Int {
 func Hash(k *big.Int) []byte {
 	hasher := sha256.New()
 	return hasher.Sum(k.Bytes())
+}
+
+type Person struct {
+	p      *big.Int
+	g      *big.Int
+	self   *big.Int
+	other  *big.Int
+	secret *big.Int
+	key    []byte
+}
+
+func NewPerson() *Person {
+	self := Rand(p)
+	return &Person{p: p, g: g, self: self}
+}
+
+func (pers *Person) Send() *big.Int {
+	return Pow(pers.g, pers.self, pers.p)
+}
+
+func (pers *Person) Receive(b *big.Int) {
+	pers.other = b
+	pers.secret = Pow(Pow(pers.g, pers.self, pers.p), pers.other, pers.p)
+	pers.key = Hash(pers.secret)[:16]
+}
+
+func (pers *Person) Encrypt(msg []byte) []byte {
+	iv := make([]byte, 16)
+	rand.Reader.Read(iv)
+	ct, err := aesmodes.EncryptCBCWithIV(pers.key, iv, msg)
+	if err != nil {
+		panic(err)
+	}
+	return ct
 }
--- dh/dh_test.go
+++ dh/dh_test.go
@@ -1,6 +1,7 @@
 package dh
 
 import (
+	"../aesmodes"
 	"testing"
 )
 
@@ -13,5 +14,42 @@ func Test(t *testing.T) {
 	s := Pow(B, a, p)
 	if r.Cmp(s) != 0 {
 		t.Fail()
+	}
+}
+
+func TestMITM(t *testing.T) {
+	alice := NewPerson()
+	bob := NewPerson()
+
+	//A := alice.Send()
+	bob.Receive(p)
+
+	//B := bob.Send()
+	alice.Receive(p)
+
+	secreta := Hash(Pow(alice.Send(), p, p))[:16]
+	secretb := Hash(Pow(bob.Send(), p, p))[:16]
+
+	//act := alice.Encrypt([]byte("tell me about the library"))
+	msga := []byte("Tell me about the library")
+	msgb := []byte("Hello Alexa")
+	cta := alice.Encrypt(msga)
+	ctb := bob.Encrypt(msgb)
+
+	resa, err := aesmodes.DecryptCBC(secreta, cta)
+	resb, err := aesmodes.DecryptCBC(secretb, ctb)
+	if err != nil {
+		panic(err)
+	}
+
+	for i, _ := range resa {
+		if resa[i] != msga[i] {
+			t.Fail()
+		}
+	}
+	for i, _ := range resb {
+		if resb[i] != msgb[i] {
+			t.Fail()
+		}
 	}
 }
